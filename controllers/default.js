7e8911d3415f0592d003d2bd824748ec31b92642
Jaaz7 <jaazielvale@hotmail.com>
2023-09-02 15:26:51 +0200

    change cols to 93 on index.html and default.js

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 92,
+            cols: 93,
             rows: 37,
             cwd: process.env.PWD,
             env: process.env
