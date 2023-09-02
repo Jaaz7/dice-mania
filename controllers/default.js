169ace1939fa7fa4d851a1731ea6d79d1f549f95
Jaaz7 <jaazielvale@hotmail.com>
2023-09-02 15:14:45 +0200

    change cols in default.js

--- controllers/default.js
+++ controllers/default.js
@@ -18,8 +18,8 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 80,
-            rows: 24,
+            cols: 98,
+            rows: 37,
             cwd: process.env.PWD,
             env: process.env
         });
